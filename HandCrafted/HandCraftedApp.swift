ac1e582c227f9870c2de431680287bbd3bc5a278
Kirill Neskoromnyy <kneskoromny@gmail.com>
2024-06-16 13:34:03 +0200

    Initial Commit

---
+++ HandCrafted/HandCraftedApp.swift
@@ -0,0 +1,17 @@
+//
+//  HandCraftedApp.swift
+//  HandCrafted
+//
+//  Created by Кирилл Нескоромный on 16.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct HandCraftedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
